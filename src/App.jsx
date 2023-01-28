ba677b19f77685b0703db4c379aa27ec4072474a
OlgaVysochanska <olavysochanska@gmail.com>
2023-01-28 23:50:26 +0200

    loader edit, api into folder services

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
-import axios from 'axios';
 import { Audio } from 'react-loader-spinner';
+
+import { searchPosts } from 'services/API';
 
 import { Searchbar } from 'components/Searchbar/Searchbar';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
@@ -21,22 +22,22 @@ export class App extends Component {
   componentDidUpdate(prevProps, prevState) {
     const { keyWord, page } = this.state;
     if (prevState.keyWord !== keyWord || prevState.page !== page) {
-      const API_KEY = '31910898-50bf1f8c70306f6b7b25ce5eb';
-      this.setState({ loading: true });
+      this.fetchPosts();
+    }
+  }
 
-      axios
-        .get(
-          `https://pixabay.com/api/?q=${keyWord}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-        )
-        .then(({ data }) => {
-          this.setState(({ images }) => ({
-            images: [...images, ...data.hits],
-          }));
-        })
-        .catch(error => {
-          this.setState({ error: error.message });
-        })
-        .finally(this.setState({ loading: false }));
+  async fetchPosts() {
+    try {
+      this.setState({ loading: true });
+      const { keyWord, page } = this.state;
+      const data = await searchPosts(keyWord, page);
+      this.setState(({ images }) => ({
+        images: [...images, ...data.hits],
+      }));
+    } catch (error) {
+      this.setState({ error: error.message });
+    } finally {
+      this.setState({ loading: false });
     }
   }
 
@@ -76,6 +77,8 @@ export class App extends Component {
             radius="9"
             color="green"
             ariaLabel="loading"
+            wrapperStyle=""
+            wrapperClass=""
           />
         )}
 
